6f91b543bd4adb0ce1cf0893e2c7c59f7203ab01
Nguyen Le Thanh Linh <nguyenlethanhlinh95@gmail.com>
2021-12-29 09:55:56 +0700

    [update]

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import logging
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
-
+    
     def parse(self, response):
-        pass
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            self.country_name = name
+            link = country.xpath(".//@href").get()
+            yield response.follow(url=link, callback = self.parse_country, meta={'country_name': name})
+            
+    def parse_country(self, response):
+        #co 2 table ma muon lay table thu 2
+        name = response.request.meta['country_name']
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            yield {
+                'name': name,
+                'year': year,
+                'population': population
+            }
